d4e8368a20643faaab79abac36494cddf47bd89c
abletoenable <abletoenable@gmail.com>
2020-06-20 23:18:59 +0300

    Add files via upload

---
+++ telegram webhook.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://api.telegram.org/bot{token}/{method}".format(
+    token="TOKEN",
+    method = "setWebhook"
+    #method = "deleteWebhook"
+    #method = "getWebhookinfo"
+    
+)
+
+data = {
+    "url": "WEBHOOK_URL"
+}
+
+r = requests.post(url, data = data)
+print(r.json())
